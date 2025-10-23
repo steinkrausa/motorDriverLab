cc593de560416791a201c7133e41ff5309ea9d10
Alec Steinkraus <steinkrausa@cod.edu>
2025-10-22 21:18:59 -0500

    rename py_pubsub to publisher.py and subscriber.py

--- src/py_pubsub/setup.py
+++ src/py_pubsub/setup.py
@@ -20,8 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-                'talker = py_pubsub.publisher_member_function:main',
-                'listener = py_pubsub.subscriber_member_function:main',
+                'talker = py_pubsub.publisher:main',
+                'listener = py_pubsub.subscriber:main',
 
         ],
 },
